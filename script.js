e7118fae7d22daf21b9c885769e5adffd4630f6e
otaviotrentini <otavio.trentini@escola.pr.gov.br>
2026-06-23 10:27:43 -0300

    versao 2.0

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-function mostrarMensagem(){
-  alert("Bem-vindo ao Projeto Agrinho 2026! 🌱");
-}
+// DARK MODE
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// ANIMAÇÃO AO ROLAR
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+
+  const trigger = window.innerHeight / 1.3;
+
+  cards.forEach(card => {
+
+    const cardTop = card.getBoundingClientRect().top;
+
+    if(cardTop < trigger){
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+
+  });
+
+});
+
+// MENSAGEM FORMULÁRIO
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  alert("Mensagem enviada com sucesso! 🌱");
+
+  form.reset();
+
+});
